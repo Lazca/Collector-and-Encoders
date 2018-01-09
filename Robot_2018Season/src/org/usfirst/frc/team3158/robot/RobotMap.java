33bcdc4c9799e287590705fe031fb3d98688d069
76497 <76497@10.25.247.62>
2018-01-09 17:07:43 -0600

    Shared first RobotMap

---
+++ Robot_2018Season/src/org/usfirst/frc/team3158/robot/RobotMap.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team3158.robot;
+
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	
+	//Collector Subsystem
+	public static int CollectorLeft;
+	public static int CollectorRight;
+	public static int CollectorPistonsOn;
+	public static int CollectorPistonsOff;
+	public static int CollectorLimitSwitch;
+	public static double SwitchSpeed;
+	public static double ScaleSpeed;
+	
+	
+}
